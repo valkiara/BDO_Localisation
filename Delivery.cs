60589a19ff97b9474a2b0797ed756eae58e66a55
Rati Agdgomelashvili <ragdgomelashvili@bdo.ge>
2019-08-20 18:30:27 +0400

    Delivery-ს მიხედვით

--- Delivery.cs
+++ Delivery.cs
@@ -343,6 +343,31 @@ namespace BDO_Localisation_AddOn
                     }
                 }
 
+                //Use Waybill ID and Number Update
+                if ((BusinessObjectInfo.EventType == SAPbouiCOM.BoEventTypes.et_FORM_DATA_ADD || BusinessObjectInfo.EventType == SAPbouiCOM.BoEventTypes.et_FORM_DATA_UPDATE)
+                        && BusinessObjectInfo.ActionSuccess == true && BusinessObjectInfo.BeforeAction == false)
+                {
+                    CommonFunctions.StartTransaction();
+
+                    SAPbouiCOM.DBDataSource DocDBSource = oForm.DataSources.DBDataSources.Item(0);
+                    string DocEntry = DocDBSource.GetValue("DocEntry", 0);
+                    string ObjType = DocDBSource.GetValue("ObjType", 0);
+
+                    string WblId = oForm.DataSources.UserDataSources.Item("BDO_WblID").ValueEx;
+                    string WblNum = oForm.DataSources.UserDataSources.Item("BDO_WblNum").ValueEx;
+
+                    JournalEntry.UpdateJournalEntryWblIdAndNumber(DocEntry, ObjType, WblId, WblNum, out errorText);
+                    if (string.IsNullOrEmpty(errorText))
+                    {
+                        CommonFunctions.EndTransaction(SAPbobsCOM.BoWfTransOpt.wf_Commit);
+                    }
+                    else
+                    {
+                        Program.uiApp.MessageBox(errorText);
+                        CommonFunctions.EndTransaction(SAPbobsCOM.BoWfTransOpt.wf_RollBack);
+                    }
+                }
+
             }
         }
 
