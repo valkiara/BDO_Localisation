8c4f769bdabe68b82dd4648239365351df84d8bc
Rati Agdgomelashvili <ragdgomelashvili@bdo.ge>
2019-08-30 13:34:19 +0000

    Revert "Merged PR 35: Waybill id და number-ის დამატება journal entry-ზე და დოკუმენტებიდან შევსება
    Waybill id და number-ის დამატება journal entry-ზე და დოკუმენტებიდან შევსება
    
    Related work items: #878"

--- APInvoice.cs
+++ APInvoice.cs
@@ -429,7 +429,6 @@ namespace BDO_Localisation_AddOn
 
                 //-------------------------------------------სასაქონლო ზედნადები----------------------------------->              
                 BDO_WBReceivedDocs.setwaybillText(oForm, out errorText);
-                AddWblIDAndNumberInJrnEntry(oForm, out errorText);
                 //<-------------------------------------------სასაქონლო ზედნადები-----------------------------------
 
                 int docEntry = Convert.ToInt32(oForm.DataSources.DBDataSources.Item("OPCH").GetValue("DocEntry", 0));
@@ -989,7 +988,7 @@ namespace BDO_Localisation_AddOn
                 }
             }
 
-            //A/C Number and Waybill ID/Number Update
+            //A/C Number and Use Rate Ranges Update
             if ((BusinessObjectInfo.EventType == SAPbouiCOM.BoEventTypes.et_FORM_DATA_ADD || BusinessObjectInfo.EventType == SAPbouiCOM.BoEventTypes.et_FORM_DATA_UPDATE)
                             && BusinessObjectInfo.ActionSuccess == true && BusinessObjectInfo.BeforeAction == false)
             {
@@ -999,7 +998,9 @@ namespace BDO_Localisation_AddOn
                 string DocEntry = DocDBSource.GetValue("DocEntry", 0);
                 string ObjType = DocDBSource.GetValue("ObjType", 0);
                 string ACNumber = DocDBSource.GetValue("U_BDOSACNum", 0);
-                
+
+                string UseRateRanges = DocDBSource.GetValue("U_UseBlaAgRt", 0);
+
                 JournalEntry.UpdateJournalEntryACNumber(DocEntry, ObjType, ACNumber, out errorText);
                 if (string.IsNullOrEmpty(errorText))
                 {
@@ -1009,9 +1010,7 @@ namespace BDO_Localisation_AddOn
                 { 
                     Program.uiApp.MessageBox(errorText);
                     CommonFunctions.EndTransaction(SAPbobsCOM.BoWfTransOpt.wf_RollBack);
-                }
-
-                AddWblIDAndNumberInJrnEntry(oForm, out errorText);
+                }                
             }
 
             
@@ -1445,30 +1444,6 @@ namespace BDO_Localisation_AddOn
 
             return 0;
 
-        }
-
-        public static void AddWblIDAndNumberInJrnEntry(SAPbouiCOM.Form oForm, out string errorText)
-        {
-            CommonFunctions.StartTransaction();
-
-            SAPbouiCOM.DBDataSource DocDBSource = oForm.DataSources.DBDataSources.Item(0);
-            string DocEntry = DocDBSource.GetValue("DocEntry", 0);
-            string ObjType = DocDBSource.GetValue("ObjType", 0);
-
-            string WblId = DocDBSource.GetValue("U_BDO_WBID", 0);
-            string WblNum = DocDBSource.GetValue("U_BDO_WBNo", 0);
-
-            JournalEntry.UpdateJournalEntryWblIdAndNumber(DocEntry, ObjType, WblId, WblNum, out errorText);
-
-            if (string.IsNullOrEmpty(errorText))
-            {
-                CommonFunctions.EndTransaction(SAPbobsCOM.BoWfTransOpt.wf_Commit);
-            }
-            else
-            {
-                Program.uiApp.MessageBox(errorText);
-                CommonFunctions.EndTransaction(SAPbobsCOM.BoWfTransOpt.wf_RollBack);
-            }
         }
 
     }
